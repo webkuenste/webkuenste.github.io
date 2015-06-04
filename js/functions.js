1559011d885839c9312667636b4e22a70c19103b
WebKuenste <webkuenste@gmail.com>
2015-06-04 22:03:22 +0200

    Portfolio

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,32 @@
-$(function (){
-  
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-  var renderer = new THREE.WebGLRenderer();
+$(function() {
+
+  var moveCamera;
+  var camera;
+  var scene;
+  var renderer;
+  var groundGeometry;
+  var groundMaterial;
+  var ground;
+  var directionalLight;
+  var pointLight;
+  var building;
+  var buildingGeometry; 
+  var buildingMaterial;
+  var rowZ;
+  var rand;
+
+  moveCamera = true;
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setClearColor(0x474747); 
   scene.fog = new THREE.FogExp2(0xffffff, 0.002);
   
-  var groundGeometry = new THREE.PlaneBufferGeometry(410,410,4,4);
-  var groundMaterial = new THREE.MeshPhongMaterial({color:0x474747});
+  groundGeometry = new THREE.PlaneBufferGeometry(410,410,4,4);
+  groundMaterial = new THREE.MeshPhongMaterial({color:0x474747});
   groundMaterial.shininess = 100;
-  var ground = new THREE.Mesh(groundGeometry,groundMaterial);
+  ground = new THREE.Mesh(groundGeometry,groundMaterial);
   scene.add(ground);
   ground.position.x = 140; 
   ground.position.y = 0; 
@@ -20,35 +36,33 @@ $(function (){
   camera.position.x = -2;
   camera.position.y = 24; 
   camera.position.z = -4; 
-  
   camera.rotation.y = -0.785398163;
   camera.rotation.z = -0.280;
   camera.rotation.x = -0.400;
   
-  var directionalLight = new THREE.DirectionalLight(0x474747, 6); 
+  directionalLight = new THREE.DirectionalLight(0x474747, 6); 
   directionalLight.position.set( -20, 15, 30 ); 
   scene.add(directionalLight);
   
-  var pointLight = new THREE.PointLight(0xffffff, 10, 32);
+  pointLight = new THREE.PointLight(0xffffff, 10, 32);
   scene.add(pointLight);
   
-  var building = [];
-  var buildingGeometry = new THREE.BoxGeometry(4,4,4); 
-  var buildingMaterial = new THREE.MeshPhongMaterial({color: 0x474747}); 
+  building = [];
+  buildingGeometry = new THREE.BoxGeometry(4,4,4); 
+  buildingMaterial = new THREE.MeshPhongMaterial({color: 0x474747}); 
   buildingMaterial.shininess = 100;
-  var building = [];
-  var rowZ = 0;
+  rowZ = 0;
   
-  for (r=0; r<35; r++){
+  for (r=0; r<35; r++) {
     rowZ -= 10;
     
-    for (i=0; i<35; i++){
+    for (i=0; i<35; i++) {
       building[i] = new THREE.Mesh(buildingGeometry, buildingMaterial); 
       building[i].position.x = i*10; 
       building[i].position.y = 0; 
       building[i].position.z = rowZ; 
     
-      var rand = Math.ceil(Math.random()*6+3);
+      rand = Math.ceil(Math.random()*6+3);
       building[i].scale.y = rand;
       scene.add(building[i]); 
     }
@@ -57,26 +71,40 @@ $(function (){
   render();
   
   function render(){
+
     requestAnimationFrame(render);
-      
-    camera.position.z -= 0.05;   
-    camera.position.x += 0.05;
-    pointLight.position.set(camera.position.x,camera.position.y,camera.position.z);
     
-    renderer.render(scene, camera);
+    if(moveCamera) {
+      camera.position.z -= 0.05;   
+      camera.position.x += 0.05;
+      pointLight.position.set(camera.position.x,camera.position.y,camera.position.z);
+      renderer.render(scene, camera);
+    }
+
   };
   
   $("#WebGLoutput").append(renderer.domElement);
 
+   function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+  }
 
   window.addEventListener('resize', onWindowResize, false);
 
-  function onWindowResize(){
+  $(window).scroll(function() {
 
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
+    var wScroll = $(this).scrollTop();
 
-      renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+    if(wScroll >= $('#WebGLoutput').height()) {
+      moveCamera = false;
+    } else {
+      moveCamera = true;
+    }
+
+  });
 
 });
